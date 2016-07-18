ef9d0e3bac0b62dedd02e96d03462c0c4c2274d8
Jason Stone <jason.p.stone@oracle.com>
2016-07-18 13:34:44 -0400

    Fix argument checking on imageprep.sh, along with some other fixes

--- imageprep.sh
+++ imageprep.sh
@@ -2,6 +2,11 @@
 
 set -eu
 
+if [[ $# != 2 ]]; then
+    echo "Usage: imageprep.sh PATH_TO_OVA IMAGE_NAME"
+    exit 1
+fi
+
 if ! [[ "$1" =~ \.(ova|ovf)$ ]]
  then
   echo "Invalid file argument - received $1"
@@ -9,14 +14,13 @@ if ! [[ "$1" =~ \.(ova|ovf)$ ]]
   exit 1
 fi
 
-if [ -z "$2" ]
- then
-   echo "No name for image supplied"
-   echo "Please supply an image name"
-   exit 1
+if ! [ -f $1 ]
+  then
+    echo "$1 does not exist."
+    exit 1
 fi
 
-sudo tar -zxvf $1 > /dev/null 2>&1
+sudo tar -xvf $1 > /dev/null 2>&1
 vmdkname="$(ls | grep -Po '.+(?=\.vmdk)').vmdk"
 
 sudo qemu-img convert -f vmdk -O qcow2 $vmdkname $2
@@ -24,15 +28,16 @@ sudo modprobe -r nbd
 sudo modprobe nbd max_part=8
 
 for nbd_device in /sys/class/block/nbd*; do
-    $size=`cat $nbd_device/size`
+    size=`cat $nbd_device/size`
     if [ "$size" -eq 0 ] ; then
-        $device_number = $(basename $nbd_device | grep -Po "[0-9]+$")
+        device_number=$(basename $nbd_device | grep -Po "[0-9]+$")
         sudo qemu-nbd --connect=/dev/nbd$device_number $2
+        sudo partx -a /dev/nbd$device_number
         break
     fi
 done
 
-TEMP = $(mktemp -d)
+TEMP=$(mktemp -d)
 
 sudo pvscan --cache
 sudo vgscan
@@ -41,7 +46,6 @@ sudo mount /dev/vgroot/plat_root $TEMP
 
 
 sudo chroot $TEMP /bin/bash -c "/sbin/chkconfig cloud-init-local on; /sbin/chkconfig cloud-init on; /sbin/chkconfig cloud-config on; /sbin/chkconfig cloud-final on"
-exit
 
 sudo umount $TEMP
 sudo rm -r $TEMP
